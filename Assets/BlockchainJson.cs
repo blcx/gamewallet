db0e54df00a7befebdbd3772605057d68ce624bb
unknown <jonnydjtracker@gmail.com>
2018-12-20 19:20:00 +0530

    update json code

---
+++ Assets/BlockchainJson.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class BlockchainData
+{
+    public string unconfirmedBalanceNQT;
+    public int effectiveBalanceNXT;
+    public Lessorsinfo[] lessorsInfo;
+    public string currentLessee;
+    public int currentLeasingHeightTo;
+    public string forgedBalanceNQT;
+    public string balanceNQT;
+    public string publicKey;
+    public int requestProcessingTime;
+    public Assetbalance[] assetBalances;
+    public string guaranteedBalanceNQT;
+    public Unconfirmedassetbalance[] unconfirmedAssetBalances;
+    public string currentLesseeRS;
+    public string accountRS;
+    public string name;
+    public string account;
+    public int currentLeasingHeightFrom;
+}
+[System.Serializable]
+public class Lessorsinfo
+{
+    public string currentHeightTo;
+    public string nextHeightFrom;
+    public string effectiveBalanceNXT;
+    public string nextLesseeRS;
+    public string currentLesseeRS;
+    public string currentHeightFrom;
+    public string nextHeightTo;
+}
+[System.Serializable]
+public class Assetbalance
+{
+    public string balanceQNT;
+    public string asset;
+}
+[System.Serializable]
+public class Unconfirmedassetbalance
+{
+        public string unconfirmedBalanceQNT;
+        public string asset;
+}
+
--- Assets/control.cs
+++ Assets/control.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class control : MonoBehaviour {
+
 public Text returnResponsText;
 public Text inputText;
+public string JSON;
+private BlockchainData data;
 
 
 private const string Endpoints = "http://35.231.191.140/nxt?requestType=getAccount&account=";
 
 
-
-
 	public void checkBalance()
 	{
-		 
-
-
 	WWW request = new WWW(Endpoints+inputText.text);
 	StartCoroutine(OnResponse(request));
 	}
@@ -27,15 +25,17 @@ private const string Endpoints = "http://35.231.191.140/nxt?requestType=getAccou
 private IEnumerator OnResponse(WWW req)
 {
  yield return req;
+ Debug.Log(req.text);
+JSON = req.text;
+data = JsonUtility.FromJson<BlockchainData>(JSON);
 
-
-
-returnResponsText.text = req.text;
-
+if (data != null && data.assetBalances != null && data.assetBalances.Length > 0)
+    returnResponsText.text = data.assetBalances[0].balanceQNT;
+    else
+     returnResponsText.text = "INVALID JSON!";
+ 
 
 }
-
-
 
 
 
