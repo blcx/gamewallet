55fd3789fe448a01aa3954b3c5ec284b2cd4d72b
unknown <jonnydjtracker@gmail.com>
2018-12-21 11:27:03 +0530

    balance check

--- Assets/control.cs
+++ Assets/control.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class control : MonoBehaviour {
-
-public Text returnResponsText;
+public Text DisplayText;
 public Text inputText;
-public string JSON;
-private BlockchainData data;
-
+BlockchainData data;
 
 private const string Endpoints = "http://35.231.191.140/nxt?requestType=getAccount&account=";
 
 
+
+
 	public void checkBalance()
-	{
+    { 
 	WWW request = new WWW(Endpoints+inputText.text);
 	StartCoroutine(OnResponse(request));
 	}
@@ -24,19 +23,11 @@ private const string Endpoints = "http://35.231.191.140/nxt?requestType=getAccou
 
 private IEnumerator OnResponse(WWW req)
 {
- yield return req;
- Debug.Log(req.text);
-JSON = req.text;
-data = JsonUtility.FromJson<BlockchainData>(JSON);
-
-if (data != null && data.assetBalances != null && data.assetBalances.Length > 0)
-    returnResponsText.text = data.assetBalances[0].balanceQNT;
-    else
-     returnResponsText.text = "INVALID JSON!";
- 
-
-}
-
-
-
+        yield return req;
+        data = JsonUtility.FromJson<BlockchainData>(req.text);
+        if (data.balanceNQT != null)
+            DisplayText.text = data.balanceNQT;
+        else
+            DisplayText.text = "ERROR!";
+    }
 }
